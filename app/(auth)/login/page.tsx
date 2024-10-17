7471632c5be594fb411c105541cb17dcfae90cf4
Daffa <daffaputradamar@gmail.com>
2024-10-17 12:13:47 +0700

    fix responsive

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,7 +9,7 @@ export default function Login() {
         <div className="w-full min-h-screen bg-[#5FBDFF] relative overflow-hidden">
             {/* Card container with z-20 to ensure it stays above the SVGs */}
             <div className="flex justify-center items-center min-h-screen relative z-20">
-                <Card className="py-10 px-4 w-1/4">
+                <Card className="py-10 px-4 w-full mx-4 md:w-1/4">
                     <CardHeader className="mb-2 space-y-2">
                         <CardTitle className="text-center text-2xl font-bold">Masuk ke akunmu</CardTitle>
                         <CardDescription className="font-semibold text-center">Please enter your email and password to continue</CardDescription>
